65d43dce57483203729ba428e9e87472307f25b9
Eduardo <eduardo.silva@fh.com.br>
2021-02-13 16:44:09 -0300

    master
    Alteração no método que calcula distancia.

--- structs.c
+++ structs.c
@@ -1,7 +1,11 @@
+/*
+ * gcc strucst.c -o structs
+ * ./structs
+ */
 #include <stdio.h>
 #include <math.h>
 
-typedef void (* dist) (struct Point *, struct Point *);
+typedef double (* dist) (struct Point *, struct Point *);
 
 typedef struct {
 	
@@ -12,25 +16,25 @@ typedef struct {
 
 } Point;
 
-void distance(Point *p1, Point *p2)
+double distance(Point *p1, Point *p2)
 {
 
 	double d1 = pow (p2-> x - p1 -> x, 2);
 	double d2 = pow (p2-> y - p1 -> y, 2);
 
 	double dist = sqrt (d1 + d2);
-    printf("Distance: %.2f\n", dist );
+    return dist;
 }
 
 
-void d2(Point *p1, Point *p2)
+double d2(Point *p1, Point *p2)
 {
 
 	double d1 = sqrt (p2-> x - p1 -> x);
 	double d2 = sqrt (p2-> y - p1 -> y);
 
 	double dist = pow (d1, d2);
-    printf("Distance: %.2f\n", dist );
+	return dist;
 }
 
 int main () {
@@ -46,10 +50,10 @@ int main () {
 
 	
     p1.d = &distance;
-    p1.d(&p1, &p2);
+	printf("Distance p1: %.2f\n\n", p1.d(&p1, &p2));
 
     p2.d = &d2;
-    p2.d(&p1, &p2);
+    printf("DIstance p2: %.2f\n\n", p2.d(&p1, &p2));
 
 	return 0;
 }
